e4d34fa1c50ba03da6b413bbe42cbb3bd0587c11
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2023-01-05 01:18:01 +0800

    chore: no gui

--- configs/whatsapp.ts
+++ configs/whatsapp.ts
@@ -1,11 +1,12 @@
 import qrcode from "qrcode-terminal";
-import { Client } from "whatsapp-web.js";
+import { Client, LocalAuth } from "whatsapp-web.js";
 
 import logger from "../utils/logger";
 const client = new Client({
   puppeteer: {
-    args: ["--no-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
+  authStrategy: new LocalAuth(),
 });
 
 client.on("qr", (qr) => {
