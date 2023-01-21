4c655e8335c234d9307af011e1431e92eccf3ad5
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2023-01-21 17:17:34 +0800

    fix logs

--- app/middlewares/auth.middleware.ts
+++ app/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import logger from "utils/logger";
+
+import logger from "../../utils/logger";
 
 export const isAuthenticated = (
   req: Request,
